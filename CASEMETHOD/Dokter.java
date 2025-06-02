6b86a175666b3afeaa2f6343ceb846af94ddd872
radita00 <radita.yogaanugrah2@gmail.com>
2025-06-02 08:17:55 +0700

    dokter

---
+++ CASEMETHOD/Dokter.java
@@ -0,0 +1,11 @@
+package CASEMETHOD;
+
+public class Dokter {
+    String idDokter;
+    String namaDokter;
+
+    public Dokter (String idDokter, String namaDokter){
+        this.idDokter = idDokter;
+        this.namaDokter = namaDokter;
+    }
+}
